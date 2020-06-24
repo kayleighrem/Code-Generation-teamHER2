ba8eaa684791c047751623270d68355b3f8fec3b
tachi191 <tachi191@hotmail.com>
2020-06-24 16:25:40 +0200

    account page BASIC visuals

--- src/main/java/io/swagger/api/AccountsApi.java
+++ src/main/java/io/swagger/api/AccountsApi.java
@@ -1,4 +1,3 @@
-
 package io.swagger.api;
 
 import io.swagger.annotations.*;
@@ -11,54 +10,52 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-05-20T13:24:55.413Z[GMT]")
-@Api(value = "accounts", description = "the accounts API")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-06-24T13:49:09.238Z[GMT]")
+@Api(value = "account", description = "the account API")
 public interface AccountsApi {
 
-    @ApiOperation(value = "show all accounts from user", nickname = "accountsGet", notes = "", response = Account.class, authorizations = {
-        @Authorization(value = "ApiKeyAuth")    }, tags={ "account", })
-    @ApiResponses(value = { 
-        @ApiResponse(code = 200, message = "showing selected account", response = Account.class),
-        @ApiResponse(code = 400, message = "failed", response = Account.class),
-        @ApiResponse(code = 401, message = "no account exist", response = Account.class) })
-    @RequestMapping(value = "/accounts",
-        produces = { "application/json" }, 
-        method = RequestMethod.GET)
-    ResponseEntity<Account> accountsGet(@NotNull @ApiParam(value = "", required = true) @Valid @RequestParam(value = "accounts", required = true) Account enumAccounts
-);
+    @ApiOperation(value = "show all accounts from user", nickname = "accountGet", notes = "", response = Account.class, authorizations = {
+            @Authorization(value = "ApiKeyAuth")    }, tags={ "account", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "showing selected account", response = Account.class),
+            @ApiResponse(code = 400, message = "failed", response = Account.class),
+            @ApiResponse(code = 401, message = "no account exist", response = Account.class) })
+    @RequestMapping(value = "/account",
+            produces = { "application/json" },
+            method = RequestMethod.GET)
+    ResponseEntity<Account> accountGet(@NotNull @ApiParam(value = "", required = true) @Valid @RequestParam(value = "IBAN", required = true) Account IBAN
+    );
 
 
-    @ApiOperation(value = "select type account", nickname = "accountsPost", notes = "", authorizations = {
-        @Authorization(value = "ApiKeyAuth")    }, tags={ "account", })
-    @ApiResponses(value = { 
-        @ApiResponse(code = 200, message = "selected account has be created"),
-        @ApiResponse(code = 400, message = "account already exist."),
-        @ApiResponse(code = 401, message = "failed to create account.") })
-    @RequestMapping(value = "/accounts",
-        method = RequestMethod.POST)
-    ResponseEntity<Void> accountsPost(@ApiParam(value = "Name of the new account",required=true) @PathVariable("accountName") String accountName
-,@NotNull @ApiParam(value = "", required = true) @Valid @RequestParam(value = "accounts", required = true) Account enumAccounts
-,@ApiParam(value = "the ammount you want to save *optionel") @Valid @RequestParam(value = "target saving", required = false) Long targetSaving
-);
+    @ApiOperation(value = "select type account", nickname = "accountPost", notes = "", authorizations = {
+            @Authorization(value = "ApiKeyAuth")    }, tags={ "account", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "selected account has be created"),
+            @ApiResponse(code = 401, message = "failed to create account.") })
+    @RequestMapping(value = "/account",
+            method = RequestMethod.POST)
+    ResponseEntity<Void> accountPost(@ApiParam(value = "userID",required=true) @PathVariable("userID") String userID
+            ,@NotNull @ApiParam(value = "Type of the new account", required = true) @Valid @RequestParam(value = "typeAcount", required = true) Account typeAcount
+    );
 
 
     @ApiOperation(value = "deletes a account", nickname = "deleteAccount", notes = "", authorizations = {
-        @Authorization(value = "ApiKeyAuth")    }, tags={ "account", })
-    @ApiResponses(value = { 
-        @ApiResponse(code = 200, message = "succesfully account deleted"),
-        @ApiResponse(code = 405, message = "vailed to delete the account") })
-    @RequestMapping(value = "/accounts",
-        method = RequestMethod.DELETE)
+            @Authorization(value = "ApiKeyAuth")    }, tags={ "account", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "succesfully account deleted"),
+            @ApiResponse(code = 405, message = "vailed to delete the account") })
+    @RequestMapping(value = "/account",
+            method = RequestMethod.DELETE)
     ResponseEntity<Void> deleteAccount();
 
 
     @ApiOperation(value = "Update a account", nickname = "updateAccount", notes = "", authorizations = {
-        @Authorization(value = "ApiKeyAuth")    }, tags={ "account", })
-    @ApiResponses(value = { 
-        @ApiResponse(code = 200, message = "account update success!"),
-        @ApiResponse(code = 405, message = "account update invalid") })
-    @RequestMapping(value = "/accounts",
-        method = RequestMethod.PUT)
+            @Authorization(value = "ApiKeyAuth")    }, tags={ "account", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "account update success!"),
+            @ApiResponse(code = 405, message = "account update invalid") })
+    @RequestMapping(value = "/account",
+            method = RequestMethod.PUT)
     ResponseEntity<Void> updateAccount();
 
 }
